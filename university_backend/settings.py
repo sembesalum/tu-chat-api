cfbb7e893197bd836feec15bc2b0b079219f5aab
salumsembe <salumrashidjr@gmail.com>
2025-02-17 01:09:04 +0300

    changes

--- university_backend/settings.py
+++ university_backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'django_extensions',
     'rest_framework.authtoken',  # Token authentication
     'api',  # Our app
     
